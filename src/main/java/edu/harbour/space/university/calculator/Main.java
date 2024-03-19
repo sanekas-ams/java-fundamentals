88321ec4dc62a81f8cb6a9e91ac393ba99d296a9
sanekas <aleksandr.o.sergeev@gmail.com>
2024-03-19 14:41:45 +0100

    Added logging

--- src/main/java/edu/harbour/space/university/calculator/Main.java
+++ src/main/java/edu/harbour/space/university/calculator/Main.java
@@ -1,14 +1,17 @@
 package edu.harbour.space.university.calculator;
 
 import edu.harbour.space.university.calculator.computation.Sum;
+import org.apache.log4j.Logger;
 
 import java.util.Scanner;
 
 public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
         InputReader inputReader = new CommandLineReader(new Scanner(System.in));
         InputParser parser = new InputParser();
         Calculator calculator = new Calculator(inputReader, parser);
-        System.out.println(calculator.compute(new Sum()));
+        LOGGER.info(STR."Output: \{calculator.compute(new Sum())}");
     }
 }
