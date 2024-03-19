f89615156fee2bb7693b0c62916cbbae356d71a7
sanekas <aleksandr.o.sergeev@gmail.com>
2024-03-19 13:28:52 +0100

    Added maven-jar plugin

--- src/main/java/edu/harbour/space/university/calculator/Main.java
+++ src/main/java/edu/harbour/space/university/calculator/Main.java
@@ -1,14 +1,14 @@
-import edu.harbour.space.university.calculator.Calculator;
-import edu.harbour.space.university.calculator.CommandLineReader;
-import edu.harbour.space.university.calculator.InputParser;
-import edu.harbour.space.university.calculator.InputReader;
+package edu.harbour.space.university.calculator;
+
 import edu.harbour.space.university.calculator.computation.Sum;
 
 import java.util.Scanner;
 
-void main(String[] args) {
-    InputReader inputReader = new CommandLineReader(new Scanner(System.in));
-    InputParser parser = new InputParser();
-    Calculator calculator = new Calculator(inputReader, parser);
-    System.out.println(calculator.compute(new Sum()));
+public class Main {
+    public static void main(String[] args) {
+        InputReader inputReader = new CommandLineReader(new Scanner(System.in));
+        InputParser parser = new InputParser();
+        Calculator calculator = new Calculator(inputReader, parser);
+        System.out.println(calculator.compute(new Sum()));
+    }
 }
